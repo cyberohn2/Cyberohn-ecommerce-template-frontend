1bf1871c692b75b703a2deb7db0ec2a3f59251e9
Edet John <edetjohn213@gmail.com>
2025-03-25 22:51:50 +0100

    styled: desktop header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import cartIcon from "/cart-icon.svg";
+import pizzlogo from "/pizz-logo.png";
+import searchIcon from "/search.svg";
+import hamburgerMenu from "/menu-burger.svg";
 
 const Header = () => {
   return (
-    <div>
-       
+    <div className="p-4 flex items-center">
+       <img className="mr-auto" width={100} src={pizzlogo} alt="Pizz peanott logo" />
+       <nav className="flex justify-between basis-[60%]">
+        <ul className="flex items-center gap-4">
+          {["Catalog", "Service", "About Us", "Contact Us"].map( (navItem, index) => <li key={index}>{navItem}</li>)}
+        </ul>
+        <div className="flex gap-4">
+          <button className="flex gap-2"><span>Search</span> <img width={15} src={searchIcon} alt="search icon" /></button>
+          <button className="flex gap-2"><span>Cart</span> <img className="" width={15} src={cartIcon} alt="cart icon" /></button>
+        </div>
+       </nav>
+       <img className="hidden" width={100} src={hamburgerMenu} alt="hamburger menu" />
     </div>
   )
 }
